7824b011129e6d0411aafb21d61f15df524d3e5f
Peter <peteratkinson1994@gmail.com>
2022-03-27 21:05:11 +0100

    update title for our website

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Pete's NextJS App with Github Actions &amp; S3!
         </h1>
 
         <p className={styles.description}>
